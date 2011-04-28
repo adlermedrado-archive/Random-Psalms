c34574a69de2983a3639b90b274c918a39b0da01
Adler Medrado <adlermedrado@gmail.com>
2011-04-28 09:58:50 -0300

    Updating comments

--- random_psalm.php
+++ random_psalm.php
@@ -5,7 +5,7 @@ Get an Randomic number between 1 and 150
 $psalmNumber = rand(1,150);
 
 /*
-add a 0 if the number is 1,2,3,4,5,6,7,8,9
+add a lead 0 if the number is 1,2,3,4,5,6,7,8,9
 */
 if (strlen($psalmNumber) == 1) {
 	$psalmNumber = "0{$psalmNumber}";
